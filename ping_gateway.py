fb988eac3c5a12f52ec04de20941036a10780026
KeepHittingTab <jared.cleverley@gmail.com>
2023-06-23 13:01:08 -0500

    modified:   green.ico modified:   ping_gateway.py modified:   red.ico

--- ping_gateway.py
+++ ping_gateway.py
@@ -1,58 +1,64 @@
-import pystray
-from pystray import MenuItem as item
-from PIL import Image
-import subprocess
-import threading
+import time
 import socket
+import struct
+import threading
+import ipaddress
+import os
+import re
+import subprocess
+from ping3 import verbose_ping
+from pystray import MenuItem as item
+import pystray
+from PIL import Image
+
+icon_online = Image.open('green.ico')
+icon_offline = Image.open('red.ico')
 
 def get_default_gateway():
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.connect(('8.8.8.8', 80))
-            default_gateway_ip = s.getsockname()[0]
-            return default_gateway_ip
-    except socket.error as e:
-        print(f"Error retrieving default gateway: {str(e)}")
-        return None
-
-def ping_gateway():
-    default_gateway_ip = get_default_gateway()
-    if default_gateway_ip:
-        try:
-            output = subprocess.check_output(['ping', '-n', '1', default_gateway_ip], timeout=1)
-            print(output.decode())
-            if 'TTL=' in output.decode():
-                update_status_icon('green')
+    if os.name == 'nt':  # For Windows
+        proc = subprocess.Popen('ipconfig', stdout=subprocess.PIPE)
+        while True:
+            line = proc.stdout.readline()
+            if line != b'':
+                line = line.decode('utf8').strip()
+                if "Default Gateway" in line:
+                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line) # this will grab the IP
+                    if ip:
+                        return ip[0]
             else:
-                update_status_icon('red')
-        except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
-            print(str(e))
-            update_status_icon('red')
-    else:
-        update_status_icon('red')
+                break
+    else:  # For Linux/Mac
+        proc = subprocess.Popen('route -n get default', stdout=subprocess.PIPE, shell=True)
+        while True:
+            line = proc.stdout.readline()
+            if line != b'':
+                line = line.decode('utf8').strip()
+                if line.startswith('gateway:'):
+                    return line.split(' ')[1]
+            else:
+                break
 
-def update_status_icon(color):
-    global icon
-    icon_path = 'green.ico' if color == 'green' else 'red.ico'
-    icon.icon = Image.open(icon_path)
-
-def monitor_thread():
-    while True:
-        ping_gateway()
-        threading.Event().wait(1)
-
-def exit_action(icon, item):
-    global monitoring_thread
-    monitoring_thread.stop()
+def action_exit(icon, item):
     icon.stop()
 
-menu = (item('Exit', exit_action),)
+def pinger(icon):
+    default_gateway = get_default_gateway()
+    while True:
+        try:
+            verbose_ping(default_gateway, timeout=1, count=1)
+            icon.icon = icon_online
+        except Exception:
+            icon.icon = icon_offline
+        icon.update_menu()
+        time.sleep(1)
+        new_default_gateway = get_default_gateway()
+        if default_gateway != new_default_gateway:
+            default_gateway = new_default_gateway
 
-initial_icon = Image.open('red.ico')
+def setup(icon):
+    icon.visible = True
+    threading.Thread(target=pinger, args=(icon,), daemon=True).start()
 
-icon = pystray.Icon("Ping Monitor", initial_icon, menu=menu)
+icon = pystray.Icon("name", icon_online, "My System Tray Icon", menu=pystray.Menu(item('Exit', action_exit)))
 
-monitoring_thread = threading.Thread(target=monitor_thread)
-monitoring_thread.start()
-
-icon.run()
+icon.run(setup)
